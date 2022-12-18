81bd18e71ba170cd99e6c43dd6a77973d54ace68
Rishav-12 <rishavmitra10@gmail.com>
2022-12-18 12:01:16 +0530

    :sparkles: add basic working - read text from input box

--- sketch.js
+++ sketch.js
@@ -1,5 +1,27 @@
+let speech;
+let input = '';
+let voiceOptions;
+
 function setup() {
   noCanvas();
-  var speech = new p5.Speech();
-  speech.speak('hi there');
+  speech = new p5.Speech();
+  speech.onLoad = voicesReady;
+
+  input = createInput('');
+  const btn = createButton('Read');
+  btn.mousePressed(readAloud);
+  voiceOptions = createSelect();
+
+  function voicesReady() {
+    voices = speech.voices;
+    for(let i=0; i<voices.length; i++) {
+      voiceOptions.option(voices[i].name);
+      voiceOptions.selected(voices[0].name);
+    }
+  }
+}
+
+function readAloud() {
+  speech.setVoice(voiceOptions.value());
+  speech.speak(input.value());
 }
